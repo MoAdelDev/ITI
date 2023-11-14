7213c3df6dd596b62e0beaaa71ac02756eebfdfd
madel <m.adeldev10@gmail.com>
2023-11-14 11:04:26 +0200

    JS Lab & Demoes

---
+++ JS COURSE/Lab 1/External.js
@@ -0,0 +1,16 @@
+//console.log(number1); //if keyword is var te result is undefined else if var without keyword the result is Exception Error
+//console.log(number2); //Excetion Error
+//console.log(number3); //Interpriting stopped before it arrived here. 
+//var number1=3; // Hoisted and defined in Global context
+"use strict"
+number1=4; // Hoisited and definded in Global Object
+let number2=2; // Hosisted and defined Global Context 
+const number3 = 5; // Hoisted and Not defined Global Context
+var firstName="Mohammed";
+let middleName='Adel';
+let lastName=`Abdelaziz`;
+let fullName='Mohammed Adel Mohammed Abdelaziz'
+let flag = true;
+
+console.log("This is the External JavaScript file");
+
