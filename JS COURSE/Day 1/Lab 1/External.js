0b7e6731253567be2fd26fd826213b159d816823
MoAdelDev <m.adeldev10@gmail.com>
2023-11-15 02:06:55 +0200

    Day 2

--- JS COURSE/Day 1/Lab 1/External.js
+++ JS COURSE/Day 1/Lab 1/External.js
@@ -3,7 +3,7 @@
 //console.log(number3); //Interpriting stopped before it arrived here. 
 //var number1=3; // Hoisted and defined in Global context
 "use strict"
-number1=4; // Hoisited and definded in Global Object
+var number1=4; // Hoisited and definded in Global Object
 let number2=2; // Hosisted and defined Global Context 
 const number3 = 5; // Hoisted and Not defined Global Context
 var firstName="Mohammed";
