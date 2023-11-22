61ad0f0c5f196686a64910e34984c85e61d28ef0
MoAdelDev <m.adeldev10@gmail.com>
2023-11-22 19:49:48 +0200

    Day 7

--- Java Script/Day 7/Lab/bird_shooter/scripts/bird_shooter.js
+++ Java Script/Day 7/Lab/bird_shooter/scripts/bird_shooter.js
@@ -1,5 +1,7 @@
 window.addEventListener('load',function(){
     let birdImgObj=document.querySelector("#bird");
+    let number = Math.round(Math.random() * (window.innerHeight-50 - 5) + 1) + 5;
+    birdImgObj.style.top=number+"px";
     moveRight(birdImgObj, 0);
 
     this.document.onclick=function(event){
