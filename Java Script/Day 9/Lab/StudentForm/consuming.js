83c296326e5b06c49520a54cef10f64461ecf9bf
MoAdelDev <m.adeldev10@gmail.com>
2023-11-27 21:44:18 +0200

    Day 9

--- Java Script/Day 9/Lab/StudentForm/consuming.js
+++ Java Script/Day 9/Lab/StudentForm/consuming.js
@@ -1,19 +1,28 @@
+
+let students=[];
+
 window.addEventListener('load', function(){
     let studentNameObj=document.querySelector('input[name=studentName]');
     let studentGradeObj=document.querySelector('input[name=studentGrade]');
-    let studentDepartmentObj=document.querySelector('input[name=Department]:checked');
+    let studentDepartmentObj=document.getElementsByName('Department');
     let addButtonObj=this.document.querySelector('input[type=button]');
-    let students=[];
 
+    let tableObject=this.document.querySelector('table');
+
+    let filterSelectObject=this.document.querySelector('#filter');
+    let sortSelectObject=this.document.querySelector('#sort');
+    
+    
     addButtonObj.onclick= function(event){
         event.preventDefault();
         let studentName = studentNameObj.value;
         let studentGrade = studentGradeObj.value;
-        let studentDepartment=studentDepartmentObj.value;
+        let studentDepartment=[...studentDepartmentObj].find(c=>c.checked).value;
+        
         if(validateStudentName(students, studentName)){
             if(validateGrade(studentGrade)){
                 // add student
-                students=addStudent(studentName, studentGrade, studentDepartment, students);
+                addStudent(studentName, studentGrade, studentDepartment, tableObject);
             }else{
                 alert("Enter Grade between 0 to 100");
             }
@@ -23,6 +32,14 @@ window.addEventListener('load', function(){
         }
     }
 
+    filterSelectObject.addEventListener('change', function(event){
+        filterStudents(filterSelectObject.value, tableObject);
+    });
+    sortSelectObject.addEventListener('change', function(event){
+        event.preventDefault();
+        console.log(sortSelectObject.value);
+        sortStudents(sortSelectObject.value, tableObject);
+    });
 });
 
 
--- Java Script/Day 9/Lab/StudentForm/function.js
+++ Java Script/Day 9/Lab/StudentForm/function.js
@@ -1,31 +1,85 @@
-
-const validateGrade=(grade)=>{
-    if(grade>=0 && grade<=100){
-        return true;
-    }
-    return false;
-}
-
-const validateStudentName=(students, studentName)=>{
-    if(studentName != '') {
-        for(let i in students){
-            if(students[i].name==studentName){
-                return false;
-            }
-            return true;
-        }
-    }
-    else{
-        return false;
-    }
+const validateGrade = (grade) => {
+  if (grade >= 0 && grade <= 100) {
     return true;
-}
+  }
+  return false;
+};
 
+const validateStudentName = (students, studentName) => {
+  if (studentName != "") {
+    for (let i in students) {
+      if (students[i].name == studentName) return false;
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
 
+const addStudent = (name, grade, department, tableObject) => {
+  let student = new Student(name, department, grade); // Create New Student
+  students.push(student); // Push New Student to students array
+  getStudents(students, tableObject);
+};
 
-const addStudent=(name, grade, department, students)=>{
-    let student = new Student(name, department, grade);
-    students.push(student);
-    console.log(students);
-    return students;
-}
+const getStudents = (students, tableObject) => {
+  document.querySelectorAll(".remove").forEach((element) => {
+    element.remove();
+  });
+  for (let i in students) {
+    let trObject = document.createElement("tr");
+    trObject.classList.add(students[i].department);
+    trObject.classList.add("remove");
+    
+    let tdName = document.createElement("td");
+    tdName.innerText = students[i].name;
+
+    let tdGrade = document.createElement("td");
+    tdGrade.innerText = students[i].grade;
+
+    let tdDelete = document.createElement("td");
+    let deleteBtn=document.createElement('button');
+    deleteBtn.innerText='Delete'
+    tdDelete.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', function(event){
+      delete students[i];
+      getStudents(students, tableObject);
+    });
+    trObject.appendChild(tdName);
+    trObject.appendChild(tdGrade);
+    trObject.appendChild(tdDelete);
+    tableObject.appendChild(trObject);
+  }
+};
+
+const filterStudents = (filter, tableObject) => {
+  let filterStudents = students.filter((item) => {
+    console.log(filter);
+    if (filter == "all") {
+      return true;
+    } else if (filter == "success") {
+      return item.grade >= 60;
+    } else if (filter == "fail") {
+      return item.grade <= 60;
+    }
+  });
+  getStudents(filterStudents, tableObject);
+};
+
+const sortStudents = (comapareValue, tableObject) => {
+    console.log(comapareValue);
+  if (comapareValue == "name") {
+    students.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+  } else {
+    students.sort((a, b) => a.grade - b.grade);
+  }
+  getStudents(students, tableObject);
+};
